34618892f795093a390efae442ef699740f0c3cb
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-10-07 07:39:09 +0530

    Initial Commit - Spring Boot with pre-config. dependencies
    https://start.spring.io/#!type=maven-project&language=java&platformVersion=2.5.5&packaging=jar&jvmVersion=11&groupId=com.example&artifactId=weatherMonitoringApp&name=weatherMonitoringApp&description=Demo%20project%20for%20Spring%20Boot&packageName=com.example.weatherMonitoringApp&dependencies=web,data-jpa,mysql

---
+++ src/test/java/com/example/weatherMonitoringApp/WeatherMonitoringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weatherMonitoringApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherMonitoringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
