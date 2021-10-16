da700733d82f9595e2f17bd1c51b9cb99ccd7c41
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-10-16 09:37:30 +0530

    This SpringBootServletInitializer run a SpringApplication from a traditional WAR deployment

--- src/main/java/com/example/weatherMonitoringApp/WeatherMonitoringAppApplication.java
+++ src/main/java/com/example/weatherMonitoringApp/WeatherMonitoringAppApplication.java
@@ -2,11 +2,18 @@ package com.example.weatherMonitoringApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WeatherMonitoringAppApplication {
+public class WeatherMonitoringAppApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WeatherMonitoringAppApplication.class);
+	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(WeatherMonitoringAppApplication.class, args);
 	}
 
