ff8d20019dcd87e54f40a6e5b441e5db23f44ae1
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-10-21 10:09:30 +0530

    Update MainController.java

--- src/main/java/com/example/weatherMonitoringApp/user/MainController.java
+++ src/main/java/com/example/weatherMonitoringApp/user/MainController.java
@@ -1,9 +1,6 @@
 package com.example.weatherMonitoringApp.user;
 
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
