5b8beb55bd8ee3314223c00c79f830343f46825e
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-11-01 16:22:45 +0530

    Update User.java

--- src/main/java/com/example/weatherMonitoringApp/user/User.java
+++ src/main/java/com/example/weatherMonitoringApp/user/User.java
@@ -4,76 +4,85 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 
 @Entity // This tells Hibernate to make a table out of this class
 public class User {
-	
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	private long id;
-	private String first_name;
-	private String last_name;
-	private String username;
-	private String email;
-	private String password;
-	
-	private String home_place;
-	private String favourite_place;
-	
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+    
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+    private String phone;
+    private String home_place;
+    private String fav_place;
+
+    public User() {}
+
+    public User(String name, String email, String phone, String home_place, String fav_place) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.home_place = home_place;
+        this.fav_place = fav_place;
+    }
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
-	public String getFirst_name() {
-		return first_name;
+
+	public String getName() {
+		return name;
 	}
-	public void setFirst_name(String first_name) {
-		this.first_name = first_name;
+
+	public void setName(String name) {
+		this.name = name;
 	}
-	public String getLast_name() {
-		return last_name;
-	}
-	public void setLast_name(String last_name) {
-		this.last_name = last_name;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public String getPassword() {
-		return password;
+
+	public String getPhone() {
+		return phone;
 	}
-	public void setPassword(String password) {
-		this.password = password;
+
+	public void setPhone(String phone) {
+		this.phone = phone;
 	}
+
 	public String getHome_place() {
 		return home_place;
 	}
+
 	public void setHome_place(String home_place) {
 		this.home_place = home_place;
 	}
-	public String getFavourite_place() {
-		return favourite_place;
-	}
-	public void setFavourite_place(String favourite_place) {
-		this.favourite_place = favourite_place;
-	}
-	
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", first_name=" + first_name + ", last_name=" + last_name + ", username=" + username
-				+ ", email=" + email + ", password=" + password + ", home_place=" + home_place + ", favourite_place="
-				+ favourite_place + "]";
+
+	public String getFav_place() {
+		return fav_place;
 	}
 
+	public void setFav_place(String fav_place) {
+		this.fav_place = fav_place;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", email=" + email + ", phone=" + phone + ", home_place="
+				+ home_place + ", fav_place=" + fav_place + "]";
+	}
+    
 }
